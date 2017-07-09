23f4ca3a158050833642d258404b66b6136fd969
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2017-07-09 11:41:17 +0800

    model average, LB Score: 0.56966

--- model/model_average.py
+++ model/model_average.py
@@ -14,14 +14,11 @@ sys.path.append(module_path)
 import pandas as pd
 from conf.configure import Configure
 
-elasticnet_result = pd.read_csv('elasticnet_model_result.csv')  # 0.55828
-lassolars_result = pd.read_csv('lassolars_model_result.csv')    # 0.55827
-rf_result = pd.read_csv('rf_regressor_model_result.csv')        # 0.55199
-ridge_result = pd.read_csv('ridge_model_result.csv')            # 0.53696
-xgboost_result = pd.read_csv('xgboost_submission.csv')          # 0.5637
+other_xgb_model_result = pd.read_csv('other_xgb_model_result.csv')  # 0.56784
+stacked_models_result = pd.read_csv('stacked-models.csv')           # 0.56894
+xgboost_result = pd.read_csv('xgboost_submission.csv')              # 0.5637
 
-result = pd.DataFrame({'ID': elasticnet_result['ID']})
-result['y'] = 0.4 * xgboost_result['y'] + 0.3 * elasticnet_result['y'] + 0.15 * lassolars_result['y'] + \
-              0.1 * rf_result['y'] + 0.05 * ridge_result['y']
+result = pd.DataFrame({'ID': xgboost_result['ID']})
+result['y'] = 0.55 * stacked_models_result['y'] + 0.3 * other_xgb_model_result['y'] + 0.15 * xgboost_result['y']
 
 result.to_csv(Configure.submission_path, index=False)
